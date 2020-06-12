2c6422b46061bcc2594f3467f68954011a0f1367
qundax <qunda.lim@outlook.sg>
2020-06-12 13:20:28 +0800

    added Cover component

---
+++ app/javascript/components/Cover.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import cover from '../../assets/images/cover.jpg'
+import EnterButton from './EnterButton'
+
+export default class Cover extends React.Component {
+    render() {
+        return (
+            <div className="d-flex justify-content-center align-items-center" style={{backgroundImage: `url(${cover})`, backgroundPosition: "center bottom", backgroundSize: "cover", width: "100%", height: "500px"}}>
+                <EnterButton />
+            </div>
+        );
+    }
+}
