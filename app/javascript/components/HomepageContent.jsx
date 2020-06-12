83ba402c5c2a19675a9dcaf1b3b7ff9acd350d30
qundax <qunda.lim@outlook.sg>
2020-06-12 13:23:53 +0800

    added HomepageContent component

---
+++ app/javascript/components/HomepageContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class HomepageContent extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
