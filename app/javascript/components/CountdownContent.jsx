253122fe228c06d6c65c2eafa81d096abaad236d
qundax <qunda.lim@outlook.sg>
2020-06-17 07:17:05 +0800

    updated CountdownContent component

--- app/javascript/components/CountdownContent.jsx
+++ app/javascript/components/CountdownContent.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import AnniversaryDisplay from './AnniversaryDisplay'
+import AddNewCountdown from './AddNewCountdown'
+import CountdownDisplay from './CountdownDisplay'
+
 export default class CountdownContent extends React.Component {
     render() {
         return (
-            <div></div>
+            <div className="d-flex justify-content-around mt-2 w-100" style={{paddingBottom: '2.5em', backgroundColor: '#F6F6F6'}}>
+                <div className='mt-5'>
+                    <AnniversaryDisplay />
+                    <AddNewCountdown />
+                </div>
+                <CountdownDisplay />
+            </div>
         );
     }
 }
