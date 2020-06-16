eda67eeeb94f276db7bb7e5e26573ad9660776f2
qundax <qunda.lim@outlook.sg>
2020-06-17 07:17:24 +0800

    added AnniversaryDisplay component

---
+++ app/javascript/components/AnniversaryDisplay.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class AnniversaryDisplay extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Anniversary</h1>
+                <div className='d-flex justify-content-center align-items-center' style={{height: '200px', marginBottom: '250px'}}>
+                    <h3>[Time Left] Days</h3>
+                </div>
+            </div>
+        );
+    }
+}
