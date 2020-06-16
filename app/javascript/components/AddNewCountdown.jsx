32070125e10c7c6e37b1755cc55b279a1f5d5570
qundax <qunda.lim@outlook.sg>
2020-06-17 07:26:41 +0800

    updated AddNewCountdown component

--- app/javascript/components/AddNewCountdown.jsx
+++ app/javascript/components/AddNewCountdown.jsx
@@ -5,7 +5,7 @@ import { Button } from '@blueprintjs/core'
 export default class AddNewCountdown extends React.Component {
     render() {
         return (
-            <div>
+            <div className='mb-5'>
                 <Button icon='plus' text='Add New Countdown' minimal='true' style={{fontSize: '20px'}} />
             </div>
         );
