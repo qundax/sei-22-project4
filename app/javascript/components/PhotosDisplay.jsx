eea816b00efd8dce5692658bb01a9d59faf40014
qundax <qunda.lim@outlook.sg>
2020-06-17 07:19:41 +0800

    added PhotosDisplay component

---
+++ app/javascript/components/PhotosDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class PhotosDisplay extends React.Component {
+    render() {
+        return (
+            <div style={{minHeight: '450px'}}>
+                <h3 className='mb-5'>Photos</h3>
+            </div>
+        );
+    }
+}
