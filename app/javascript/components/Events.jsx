9b42abebc9e48a36e8ca7d12c7cd136e644933b6
qundax <qunda.lim@outlook.sg>
2020-06-16 16:37:51 +0800

    added Events component

---
+++ app/javascript/components/Events.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class Events extends React.Component {
+    stringifyDate(date) {
+        return date.toString().slice(0,15);
+    }
+
+    render() {
+        const selectedDate = this.stringifyDate(this.props.date);
+
+        return (
+            <div className='mt-5'>
+                <h3>Events on {selectedDate}</h3>
+            </div>
+        );
+    }
+}
