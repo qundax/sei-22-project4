c20bb3faec12889051ed1f6f67c56a176b66bd61
qundax <qunda.lim@outlook.sg>
2020-06-16 16:30:44 +0800

    updated AppNav component

--- app/javascript/components/AppNav.jsx
+++ app/javascript/components/AppNav.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import HomepageNav from './HomepageNav'
+import PageLinks from './PageLinks'
+
 export default class AppNav extends React.Component {
     render() {
         return (
-            <div></div>
+            <div className='d-flex flex-column align-items-center w-100' style={{backgroundColor: '#FFFFFF'}}>
+                <HomepageNav />
+                <PageLinks />
+            </div>
         );
     }
 }
