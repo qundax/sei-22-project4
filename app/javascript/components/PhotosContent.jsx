240bf205528f6d015fc220016b85ec5d6811e91f
qundax <qunda.lim@outlook.sg>
2020-06-17 07:19:06 +0800

    updated PhotosContent component

--- app/javascript/components/PhotosContent.jsx
+++ app/javascript/components/PhotosContent.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import AddNewPhoto from './AddNewPhoto'
+import PhotosDisplay from './PhotosDisplay'
+
 export default class PhotosContent extends React.Component {
     render() {
         return (
-            <div></div>
+            <div className="d-flex flex-column align-items-center mt-2 w-100" style={{paddingBottom: '2.5em', backgroundColor: '#F6F6F6'}}>
+                <AddNewPhoto />
+                <PhotosDisplay />
+            </div>
         );
     }
 }
