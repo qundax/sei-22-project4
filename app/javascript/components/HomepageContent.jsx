a93afed38fc876d0c8629ded6eb21ff5f4979463
qundax <qunda.lim@outlook.sg>
2020-06-16 16:35:01 +0800

    updated HomepageContent component

--- app/javascript/components/HomepageContent.jsx
+++ app/javascript/components/HomepageContent.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import HomepageCover from './HomepageCover'
+
 export default class HomepageContent extends React.Component {
     render() {
         return (
-            <div>
-
+            <div className="d-flex flex-column align-items-center mt-2 w-100" style={{paddingBottom: "2.5em"}}>
+                <HomepageCover />
             </div>
         );
     }
