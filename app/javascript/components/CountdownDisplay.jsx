19ba4ba345ef90995f33b86f88802e5284330f11
qundax <qunda.lim@outlook.sg>
2020-06-17 07:18:14 +0800

    added CountdownDisplay component

---
+++ app/javascript/components/CountdownDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class CountdownDisplay extends React.Component {
+    render() {
+        return (
+            <div className='mt-5'>
+                <h3>Event Countdowns</h3>
+            </div>
+        );
+    }
+}
