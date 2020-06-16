2865629dfb1e6cc01c11abc7727ec343b7326e57
qundax <qunda.lim@outlook.sg>
2020-06-16 16:37:36 +0800

    added CalendarContent component

---
+++ app/javascript/components/CalendarContent.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { DatePicker } from "@blueprintjs/datetime"
+
+import Events from './Events'
+
+export default class CalendarContent extends React.Component {
+    constructor() {
+        super();
+        this.state = {date: new Date(Date.now())};
+    }
+
+    setDate(value) {
+        this.setState({date: value})
+    }
+
+    render() {
+        return (
+            <div className="d-flex justify-content-around mt-2 w-100" style={{paddingBottom: '2.5em'}}>
+                <DatePicker className='datepicker mt-5' highlightCurrentDay='true' onChange={(value)=>{this.setDate(value)}} />
+                <Events date={this.state.date} />
+            </div>
+        );
+    }
+}
