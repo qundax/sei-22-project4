0d293f0b44454787bd9d7962be25d06d076912e2
qundax <qunda.lim@outlook.sg>
2020-06-17 09:29:39 +0800

    minor changes to migrations

--- db/migrate/20200616233044_create_couples.rb
+++ db/migrate/20200616233044_create_couples.rb
@@ -2,8 +2,8 @@ class CreateCouples < ActiveRecord::Migration[6.0]
   def change
     create_table :couples do |t|
       t.timestamp :anniversary
-      t.references :user1, index: true, foreign_key: true
-      t.references :user2, index: true, foreign_key: true
+      t.integer :user1, index: true, foreign_key: true
+      t.integer :user2, index: true, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_105241) do
+ActiveRecord::Schema.define(version: 2020_06_17_011759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "countdowns", force: :cascade do |t|
+    t.bigint "couple_id", null: false
+    t.text "name"
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["couple_id"], name: "index_countdowns_on_couple_id"
+  end
+
+  create_table "couples", force: :cascade do |t|
+    t.datetime "anniversary"
+    t.integer "user1"
+    t.integer "user2"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user1"], name: "index_couples_on_user1"
+    t.index ["user2"], name: "index_couples_on_user2"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.bigint "couple_id", null: false
+    t.text "name"
+    t.datetime "date"
+    t.text "description"
+    t.boolean "is_couple_event"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["couple_id"], name: "index_events_on_couple_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.bigint "couple_id", null: false
+    t.text "image_url"
+    t.text "caption"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["couple_id"], name: "index_photos_on_couple_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +68,7 @@ ActiveRecord::Schema.define(version: 2020_06_16_105241) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "countdowns", "couples"
+  add_foreign_key "events", "couples"
+  add_foreign_key "photos", "couples"
 end
